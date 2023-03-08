6f01bed8339c1152a1f82ae173c2272963f7faae
Isabelle <ihkenny01@gmail.com>
2023-03-08 03:09:58 -0500

    Isabelle decode

--- MillerEncode.py
+++ MillerEncode.py
@@ -9,8 +9,20 @@ def encode_password(password):
             encoded_digit = str((int(digit) + 3)%10)
             encoded_password += encoded_digit
 
-        print("Your password has been encoded and stored!")
+        print("Your password has been encoded and stored!\n")
         return encoded_password
+
+
+#Isabelle - decode
+def decode_password(password):
+        encoded = [int(x) for x in str(password)]
+        for i in range(8):
+            temp = encoded[i]
+            encoded[i] = temp
+        decodedPassword = ''.join(map(str, encoded))
+
+        print(f'The encoded password is {password}, and the original password is {decodedPassword}\n')
+        return decodedPassword
 
 def main():
     while True:
@@ -19,8 +31,10 @@ def main():
         if menu_select == 1:
             user_password = input("Please enter your password to encode: ")
             encoded_user_password = encode_password(user_password)
+
+        #Isabelle - decode
         elif menu_select == 2:
-            print("This functionality has not been implemented yet.")
+            decode_password(encoded_user_password)
         elif menu_select == 3:
             break
         else:
